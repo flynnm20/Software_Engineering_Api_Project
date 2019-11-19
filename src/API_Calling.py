cf4eda15ef53420c06aa73c17e94644c26831dd5
flynnm20 <46966461+flynnm20@users.noreply.github.com>
2019-11-19 11:16:11 +0000

    Generate ScatterPlot
    The program can now generate a scatter plot from the data that is generated.

--- src/API_Calling.py
+++ src/API_Calling.py
@@ -1,66 +1,64 @@
+import peewee
+from github import Github
+import plotly.express as px
 import math
 import json
+import matplotlib.pyplot as plt
+import numpy as np
 from enum import unique
-from github import Github
-import peewee
-
+import pandas
 
 def getRepoLanguagesCount(repos):
- retLang = []
- num = 0
- for r in repos:
-  lang = r.get_languages()
-  for l in lang:
-   if l not in retLang:
-    retLang.append(l)
- for n in retLang:
-  num = num +1
- return num
+    retLang = []
+    num = 0
+    for r in repos:
+        lang = r.get_languages()
+        for l in lang:
+            if l not in retLang:
+                retLang.append(l)
+    for n in retLang:
+        num = num + 1
+    return num
 
 
 def get_avg_of_repo(repositories):
- num = 0
- avg = 0
- for repo in repositories:
-  if num == 800:
-   break
-  avg = avg + repo.stargazers_count
-  num = num + 1
- return avg
+    num = 0
+    avg = 0
+    for repo in repositories:
+        if num == 800:
+            break
+        avg = avg + repo.stargazers_count
+        num = num + 1
+    return avg
 
 
+# access token for github
+g = Github("0fc78182ed7267d879b1074064e549d57eefdaed")
 
-g = Github("Github Token") # access token for github
-
-google = g.get_organization("Google")
+google = g.get_organization("Facebook")
 members = google.get_members()
 num = 0
-data = {}
-data["points"] = []
+avg_Str = []
+nm_Lng = []
 for mem in members:
- repos = mem.get_repos()
- avgStars = get_avg_of_repo(repos)
- langKnown = getRepoLanguagesCount(repos)
- data['points'].append({'avgStr' : avgStars, 'lngKnwn':langKnown})
- print("Users avg star rating is =",avgStars, "Users number of languages known is ",langKnown)
- if num > 10:
-  break
- print(mem)
- num=num+1
-with open['points.txt','w+'] as outfile:
- json.dump(data, outfile)
+    repos = mem.get_repos()
+    avgStars = get_avg_of_repo(repos)
+    langKnown = getRepoLanguagesCount(repos)
+    avg_Str.append(avgStars)
+    nm_Lng.append(langKnown)
+    print("Gotone")
+    num = num+1
+    if num >= 10:
+        break
 
+print("Out of loop")
 
-'''
-avgPy = 0
-avgJS =0
-avgJ = 0 #var for the average star of python contributures
-repositoriesPython = g.search_repositories(query='language:python') #get the repositories that use python
-repositoriesJavaScript = g.search_repositories(query='language:javascript') #get the repositories that use python
-repositoriesJAVA = g.search_repositories(query='language:java') #get the repositories that use python
+N = 50
+x=avg_Str
+y=nm_Lng
+colors = 100
+area = (30 * np.random.rand(N))**2  # 0 to 15 point radii
+print("Dataset generated")
 
-avgPy = get_avg_of_repo(repositoriesPython)/800
-avgJ = get_avg_of_repo(repositoriesJAVA)/800
-print(math.ceil(avgPy))
-'''
-
+plt.scatter(x,y)
+plt.show()
