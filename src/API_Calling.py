a46f7fa407f2828abe856ed3334e387b6847f813
flynnm20 <46966461+flynnm20@users.noreply.github.com>
2019-11-28 16:26:02 +0000

    Finished version
    Finished project. Removed unnecessary lines of code and added comments to improve readability

--- src/API_Calling.py
+++ src/API_Calling.py
@@ -1,19 +1,8 @@
 import time
-import peewee
 from github import Github
-import math
-import json
-import matplotlib.pyplot as plt
 from plotly.offline import plot
 import plotly.express as px
-from plotly.subplots import make_subplots
-import operator
-from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-from matplotlib.figure import Figure
-import numpy as np
-from enum import unique
-from io import StringIO
-from flask import Response
+import plotly.graph_objects as go
 import pandas as pd
 
 #create a strongest language view where we get the most popular language and compare the repos that their in.
@@ -27,33 +16,37 @@ def appToData(tmpLangData, langData):
             langData[l] = langData[l] + tmpLangData[l]
     return langData
 
+# Class for orginizations and is used to store data regarding the orginiations.
 class Org:
     def __init__(self, name):
         self.name = name
-        self.langData = {}
+        self.langData = {} # dic for the languages and the sum of the stars used.
         self.members = []
         self.fig = None
         self.mostUsedLang = None
-        self.mostUsedLangStar = 0
-        self.AvgSList = []
-        self.LnKnList = []
+        self.mostUsedLangStar = 0 # calculated at the end
+        self.AvgSList = [] # x axis of the scatter plot
+        self.LnKnList = [] # y axis of the scatter plot
         self.org = g.get_organization(self.name)
         self.membersList = self.org.get_members()
         self.num = 0
+        self.labels = []
+        self.values = []
         # get avg stars and lang used for each member of an organization up to a set limit
         df = pd.DataFrame
-        for m in self.membersList:
+        for m in self.membersList: # get teach dot for the values
             repos = m.get_repos()
             tmpLangData = getRepoLanguagesCount(repos)
             self.langData = appToData(tmpLangData, self.langData)
             self.langKnown = len(tmpLangData)
-            self.AvgSList.append(get_avg_of_repo(repos))
-            self.LnKnList.append(self.langKnown)
+            self.AvgSList.append(get_avg_of_repo(repos)) # append new x value
+            self.LnKnList.append(self.langKnown) # append new y value
             self.num = self.num + 1
-            if self.num >= 1:
+            if self.num >= 20:
                 break
-        self.mostUsedLang = max(self.langData, key=self.langData.get)
+        self.mostUsedLang = max(self.langData, key=self.langData.get) # assign most used language
 
+# Get the unique instances of languages for all the repos.
 def getRepoLanguagesCount(repos):
     retLang = {} # dict of all lang listed and the stars associated with that language
     num = 0
@@ -81,25 +74,20 @@ def get_avg_of_repo(repositories):
     return (avg/num)
 
 
-
-
-
 #Main
 # access token for github
-g = Github("AccessToken")
+g = Github("ACCESS TOOKEN")
 listOfOrgs = ["Facebook", "amzn", "Apple", "Google", "IBM"]
 most_used_lang = []
 most_used_lang_vals = []
-fig = plt.figure()
 n = 0
-colours = ["b", "g", "r", "c", "y"]
-plt.figure(1)
 aS= []
 lK = []
 index = []
-for i in listOfOrgs:
+for i in listOfOrgs: # cycle through each specified 
     tmpOrg = Org(i)
-    strings = [tmpOrg.mostUsedLang,'/',tmpOrg.name]
+    print(i)
+    strings = [tmpOrg.mostUsedLang,'/',tmpOrg.name] # used as label for the barchart
     barLabel = ''.join(strings)
     most_used_lang.append(barLabel)
     most_used_lang_vals.append(tmpOrg.langData[tmpOrg.mostUsedLang])
@@ -110,13 +98,11 @@ for i in listOfOrgs:
     for b in tmpOrg.LnKnList:
         lK.append(b)
     n = n+1
-
-scatData = pd.DataFrame({"Org":index, "Average Stars": aS, "Languages Known":lK})
+#Data has been gathered and now needs to be ploted
+scatData = pd.DataFrame({"Org":index, "Average Stars": aS, "Languages Known":lK}) # create a pd dataframe for use in ploting
 barData = pd.DataFrame({"Org":listOfOrgs,"UsedLang":most_used_lang, "Vals":most_used_lang_vals})
-
-
-fig = px.scatter(scatData,x="Average Stars",y = "Languages Known", color="Org")
-plot(fig)
-time.sleep(30)
+fig = px.scatter(scatData,x="Average Stars",y = "Languages Known", color="Org") # calculate the dots and set the colours based on organizations
+plot(fig) # plot the scatter plot
+time.sleep(30) # this is to allow users time to allow the program to run if needed.
 figBar = px.bar(barData, x = "UsedLang", y = "Vals", color="Org")
-plot(figBar)
+plot(figBar) # plot the bar chart
